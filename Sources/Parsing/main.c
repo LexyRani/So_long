95b6fae6e4068bb6b4b2929c06aa02c98a1ef173
Alix CERALINE <aceralin@e2r5p1.clusters.42paris.fr>
2023-02-17 22:44:03 +0100

    Norminette main.c ok

--- Sources/Parsing/main.c
+++ Sources/Parsing/main.c
@@ -6,40 +6,33 @@
 /*   By: aceralin <aceralin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 13:20:07 by aceralin          #+#    #+#             */
-/*   Updated: 2023/02/16 14:43:00 by aceralin         ###   ########.fr       */
+/*   Updated: 2023/02/17 22:41:16 by aceralin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../Includes/so_long.h"
-/*
-void	ft_file_to_image(void *mlx_ptr)
-{
-	void *img;
-	char	*img_path;
-}*/
-
 
 int	ft_arg_is_not_ber(char *s1, char *extension)
 {
 	int		i;
 
 	i = (int)ft_strlen(s1) - 1;
-	while(s1[i] && s1[i] != '.')
+	while (s1[i] && s1[i] != '.')
 		i--;
-	if(i <= 0)
-		return(1);
-	if(ft_strcmp(s1 + i, extension))
-		return(1);
-	return(0);
+	if (i <= 0)
+		return (1);
+	if (ft_strcmp(s1 + i, extension))
+		return (1);
+	return (0);
 }
 
-void 	ft_check_map(t_game *game, t_map **map, int fd)
+void	ft_check_map(t_game *game, t_map **map, int fd)
 {
 	t_map	*extract;
-	char 		*ptr;
+	char	*ptr;
 
 	ptr = get_next_line(fd);
-	while(ptr)
+	while (ptr)
 	{
 		extract = ft_lstnew(ptr);
 		game->head_map = extract;
@@ -56,27 +49,26 @@ void 	ft_check_map(t_game *game, t_map **map, int fd)
 int	main(int argc, char *argv[])
 {
 	t_map	**map;
-	int			fd;
-	t_game		*game;
-	t_map		*lst;
+	int		fd;
+	t_game	*game;
+	t_map	*lst;
 
 	game = (void *)malloc(sizeof(t_game));
 	lst = NULL;
 	map = &lst;
-	//game->img_coin = 0;
 	if (argc != 2)
 		ft_exit(NULL, "Error");
-	if((ft_arg_is_not_ber(argv[1], ".ber")))
-		ft_exit(NULL,"Error: Your file is not .ber");
+	if ((ft_arg_is_not_ber(argv[1], ".ber")))
+		ft_exit(NULL, "Error: Your file is not .ber");
 	fd = open(argv[1], O_RDONLY);
-	if(fd == -1)
+	if (fd == -1)
 		ft_exit(NULL, "Error to open the file");
 	ft_check_map(game, map, fd);
 	ft_map_is_valid(game, map);
 	display_the_map(game);
 	free_game(game);
 	dprintf(1, "%s\n", "--- EXITED HERE ---\n");
-	ft_exit(*map,"");
+	ft_exit(*map, "");
 }
 
 //--leak-check=full --show-leak-kinds=
